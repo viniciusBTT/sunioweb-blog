21ffb9dc6ea672c8d15bb71becf71e0e457d904d
Vinicius Barros Teixeira <104240088+viniciusBTT@users.noreply.github.com>
2024-02-26 19:05:51 -0300

    styles

--- src/main/java/br/com/blog/sunioweb/controller/HomeController.java
+++ src/main/java/br/com/blog/sunioweb/controller/HomeController.java
@@ -1,7 +1,9 @@
 package br.com.blog.sunioweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
