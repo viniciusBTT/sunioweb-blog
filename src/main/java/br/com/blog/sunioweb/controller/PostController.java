0796e77eb6db2c49016bc0f1b183e073c5c8495e
Vinicius Barros Teixeira <vini.barrost@gmail.com>
2024-03-17 19:58:58 -0300

    ajustes de autenticação

--- src/main/java/br/com/blog/sunioweb/controller/PostController.java
+++ src/main/java/br/com/blog/sunioweb/controller/PostController.java
@@ -1,7 +1,6 @@
 package br.com.blog.sunioweb.controller;
 
-import br.com.blog.sunioweb.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
+import br.com.blog.sunioweb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,18 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
-public class AuthController {
-
+@RequestMapping("/posts")
+public class PostController {
     @Autowired
-    private UserService userService;
-
+    private PostService postService;
 
     @GetMapping
-    public String auth(HttpServletRequest request)
-    {
-        System.out.println("daiowdawkdnjawidjaidjaiodjaw");
-        return "/auth/login";
+    public String posts(Model model){
+        model.addAttribute("posts", postService.findAll());
+        return "/post/posts";
     }
-
 }
--- src/main/java/br/com/blog/sunioweb/service/PostService.java
+++ src/main/java/br/com/blog/sunioweb/service/PostService.java
@@ -1,10 +1,18 @@
 package br.com.blog.sunioweb.service;
 
+import br.com.blog.sunioweb.model.Post;
+import br.com.blog.sunioweb.model.User;
 import br.com.blog.sunioweb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
-
+    @Autowired
     private PostRepository repository;
+
+    public List<Post> findAll(){return repository.findAll();}
+
 }
