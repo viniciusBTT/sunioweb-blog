abfa17f2d88c807a1f775c5f633cad6bb5e2657e
Vinicius Barros Teixeira <vini.barrost@gmail.com>
2024-03-27 03:30:05 -0300

    UPDATE/ fragments

---
+++ src/main/resources/static/assets/js/subject.js
@@ -0,0 +1,32 @@
+function  handleBtnSubjectModal(id){
+    url =  `/subjects/save${id ? '/'+id : ''}`
+
+    fetch(url, {
+        method: 'GET'
+    })
+    .then(response => {
+        // Retorna o corpo da resposta como texto
+        return response.text();
+    })
+    .then(html => {
+        Swal.fire({
+            title: "Cadastro de postagem",
+            html: html,
+            showConfirmButton: false,
+        })
+
+    })
+    .catch(error => {
+        Swal.fire({
+                    icon: 'error',
+                    position: "center",
+                    text: 'Erro ao buscar o post',
+                    timerProgressBar: true,
+                    background: '#f1f1f1 ',
+                    backdrop: "rgba(0, 0, 0, 0)" ,
+                })
+        console.error('Erro', error);
+    });
+
+
+}
